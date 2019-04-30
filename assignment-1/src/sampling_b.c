d4cba7859ac45cf8cfb3894e6156a4217c5a8c2b
Alexandros Sahinis <alexsah97@gmail.com>
2019-05-01 02:31:25 +0300

    Complete second part
    The second part is the same as the first one, except the way that
    the sleep duration is calculated. It is adapted to the slight delay
    of each sampling interval, by dynamicaly decreasing the sleep time
    to make up for the time lost.

---
+++ assignment-1/src/sampling_b.c
@@ -0,0 +1,76 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+void end_program();
+void init(char**);
+void run();
+useconds_t get_sample();
+void end_program();
+
+int samples, duration;
+useconds_t* intervals;
+useconds_t dt, delta;
+struct timeval start;
+
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stdout, "Not enough arguments given\n");
+        return 0;
+    }
+    signal(SIGALRM, end_program);
+    init(argv);
+    run();
+}
+
+void init(char **argv) {
+    duration = atoi(argv[1]);
+    float interval = atof(argv[2]);
+    int count = (useconds_t) (duration/interval);
+    // allocate memory for storing the interval values
+    intervals = (useconds_t*) malloc(count * sizeof(useconds_t));
+    dt = (useconds_t) (atof(argv[2]) * pow(10, 6));
+}
+
+void run() {
+    // starting time
+    gettimeofday(&start, NULL);
+    samples = 0;
+    delta = dt;
+    // set alarm to know when to exit the sampling procedure
+    alarm(duration);
+    while (1){
+        usleep(2 * dt - delta);
+        intervals[samples++] = get_sample();        
+    }
+}
+
+useconds_t get_sample() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    delta = (tv.tv_sec - start.tv_sec) * pow(10, 6) + tv.tv_usec - start.tv_usec;
+    start = tv;
+    return delta;
+}
+
+void end_program() {
+    printf("Writing results to file...\n");
+    FILE *fptr;
+    fptr = fopen("../log/sampling_b.csv","w");
+    if (fptr == NULL){
+     printf("Something went wrong\n");
+     _exit(1);
+    }
+    fprintf(fptr, "sample,interval,duration\n");
+    float sum = 0;
+    for(int j = 0; j < samples; j++) {
+        sum += intervals[j] / pow(10, 6);
+        fprintf(fptr, "%d,%d,%f\n", j, intervals[j], sum);
+    }
+    fclose(fptr);
+    exit(0);
+}
